b7f463ed294c14af271c7130518ed66c332a5ea7
meimeuiii <152019926+meimeuiii@users.noreply.github.com>
2024-07-22 16:30:29 +0300

    ghfg

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import random
+
 from PyQt5.QtWidgets import *
+
+import database
 
 app = QApplication([])
 window = QWidget()
@@ -13,8 +17,9 @@ varl2_btn = QRadioButton("2")
 varl3_btn = QRadioButton("3")
 varl4_btn = QRadioButton("4")
 answer_btn = QPushButton("Відповісти")
-
+res_lbl = QLabel("Результат")
 group = QGroupBox("Варіанти відповідей")
+next_quest_btn = QPushButton("Наступне запитання")
 
 main_line = QVBoxLayout()
 horizontal_line1 = QHBoxLayout()
@@ -26,45 +31,52 @@ horizontal_line1.addWidget(min_lbl)
 main_line.addLayout(horizontal_line1)
 main_line.addWidget(quest_lbl)
 
+
+
 group_main_line = QVBoxLayout()
 group_main_line.addWidget(varl1_btn)
 group_main_line.addWidget(varl2_btn)
 group_main_line.addWidget(varl3_btn)
 group_main_line.addWidget(varl4_btn)
+group_main_line.addWidget(res_lbl)
 group.setLayout(group_main_line)
 main_line.addWidget(group)
 main_line.addWidget(answer_btn)
+main_line.addWidget(next_quest_btn)
+answers = [varl1_btn, varl2_btn, varl3_btn, varl4_btn]
+
+def set_quest():
+    random.shuffle(answers)
+    current_question = database.question[database.nomer]
+    quest_lbl.setText(current_question["запитання"])
+    answers[0].setText(current_question["Правильна відповідь"])
+    answers[1].setText(current_question["Не правильна відповідь1"])
+    answers[2].setText(current_question["Не правильна відповідь2"])
+    answers[3].setText(current_question["Не правильна відповідь3"])
+set_quest()
+res_lbl.hide()
+next_quest_btn.hide()
 
 
 
+def ans_func():
+    if answers[0].isChecked():
+        res_lbl.setText("Правильно")
+    else:
+        res_lbl.setText("не правильно")
+    answers[0].hide()
+    answers[1].hide()
+    answers[2].hide()
+    answers[3].hide()
+    res_lbl.show()
+    next_quest_btn.show()
+    answer_btn.hide()
 
+answer_btn.clicked.connect(ans_func)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+window.setLayout(main_line)
+window.show()
+app.exec()
 
 
 
